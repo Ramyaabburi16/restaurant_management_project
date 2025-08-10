9c0e889d9cb2c46bee9cb0ed1db4ca7a697757ef
Ramya Abburi <RAMYAABBURI74@GMAIL.COM>
2025-08-10 19:26:48 +0530

    21758

--- account/models.py
+++ account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADDE)
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+ 
